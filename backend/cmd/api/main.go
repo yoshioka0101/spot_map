01602453d53c44c1e74af2c73458e37b7d7461eb
yoshioka0101 <yuuki0205nano@gmail.com>
2025-03-11 18:48:21 +0900

    feat:main.goのディレクトリ変更

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "log"
+	"github.com/yoshioka0101/spot_map/internal/server"
+)
+
+func main() {
+    fmt.Println("Hello, World!")
+
+	r := server.NewRouter()
+
+	// サーバーの起動
+	log.Println("Starting server on :8000")
+	if err := r.Run(":8000"); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
